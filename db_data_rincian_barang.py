16d3a78797c0f0f66f59c39b07de164999256c2e
Muhammad Zakii Rofii <57617074+zakii-hub@users.noreply.github.com>
2023-02-13 02:36:45 +0700

    Penambahan Kelengkapan Data sqlite3

--- db_data_rincian_barang.py
+++ db_data_rincian_barang.py
@@ -4,11 +4,11 @@ c = conn.cursor()
 
 
 def create_table_data_rincian_barang():
-	c.execute('CREATE TABLE IF NOT EXISTS data_rincian_barang(id_rincian_barang TEXT UNIQUE PRIMARY KEY,nama_rincian_barang TEXT, status_rincian_barang TEXT, longitude_rincian_barang INTEGER, latitude_rincian_barang INTEGER,alamat_rincian_barang TEXT)')
+	c.execute('CREATE TABLE IF NOT EXISTS data_rincian_barang(id_barang TEXT UNIQUE PRIMARY KEY,nama_barang TEXT, panjang_barang INTEGER, lebar_barang INTEGER, tinggi_barang INTEGER, massa_barang INTEGER, volumetrik_pengiriman INTEGER, jumlah_barang INTEGER)')
 
 
-def add_data_rincian_barang(id_rincian_barang,nama_rincian_barang,status_rincian_barang,longitude_rincian_barang,latitude_rincian_barang,alamat_rincian_barang):
-	c.execute('INSERT INTO data_rincian_barang(id_rincian_barang,nama_rincian_barang,status_rincian_barang,longitude_rincian_barang,latitude_rincian_barang,alamat_rincian_barang) VALUES (?,?,?,?,?,?)',(id_rincian_barang,nama_rincian_barang,status_rincian_barang,longitude_rincian_barang,latitude_rincian_barang,alamat_rincian_barang))
+def add_data_rincian_barang(id_barang,nama_barang,panjang_barang,lebar_barang,tinggi_barang,massa_barang,volumetrik_pengiriman,jumlah_barang):
+	c.execute('INSERT INTO data_rincian_barang(id_barang,nama_barang,panjang_barang,lebar_barang,tinggi_barang,massa_barang,volumetrik_pengiriman,jumlah_barang) VALUES (?,?,?,?,?,?,?,?)',(id_barang,nama_barang,panjang_barang,lebar_barang,tinggi_barang,massa_barang,volumetrik_pengiriman,jumlah_barang))
 	conn.commit()
 
 
@@ -18,26 +18,26 @@ def view_all_data_rincian_barang():
 	return data
 
 def view_all_nama_rincian_barang():
-	c.execute('SELECT DISTINCT nama_rincian_barang FROM data_rincian_barang')
+	c.execute('SELECT DISTINCT nama_barang FROM data_rincian_barang')
 	data = c.fetchall()
 	return data
 
-def get_nama_rincian_barang(nama_rincian_barang):
-	c.execute('SELECT * FROM data_rincian_barang WHERE nama_rincian_barang="{}"'.format(nama_rincian_barang))
+def get_nama_rincian_barang(nama_barang):
+	c.execute('SELECT * FROM data_rincian_barang WHERE nama_barang="{}"'.format(nama_barang))
 	data = c.fetchall()
 	return data
 
-def get_rincian_barang_by_status(status_rincian_barang):
-	c.execute('SELECT * FROM data_rincian_barang WHERE status_rincian_barang="{}"'.format(status_rincian_barang))
-	data = c.fetchall()
+# def get_rincian_barang_by_status(status_rincian_barang):
+# 	c.execute('SELECT * FROM data_rincian_barang WHERE status_rincian_barang="{}"'.format(status_rincian_barang))
+# 	data = c.fetchall()
 
 
-def edit_data_rincian_barang(new_id_rincian_barang,new_nama_rincian_barang,new_status_rincian_barang,new_longitude_rincian_barang,new_latitude_rincian_barang,new_alamat,id_rincian_barang,nama_rincian_barang,status_rincian_barang,longitude_rincian_barang,latitude_rincian_barang,alamat_rincian_barang):
-	c.execute("UPDATE data_rincian_barang SET id_rincian_barang =?,nama_rincian_barang=?,status_rincian_barang=?,longitude_rincian_barang=?,latitude_rincian_barang=?,alamat_rincian_barang=? WHERE id_rincian_barang =? and nama_rincian_barang=? and status_rincian_barang=? and longitude_rincian_barang=? and latitude_rincian_barang=? and alamat_rincian_barang=? ",(new_id_rincian_barang,new_nama_rincian_barang,new_status_rincian_barang,new_longitude_rincian_barang,new_latitude_rincian_barang,new_alamat,id_rincian_barang,nama_rincian_barang,status_rincian_barang,longitude_rincian_barang,latitude_rincian_barang,alamat_rincian_barang))
+def edit_data_rincian_barang(new_id_barang,new_nama_barang,new_panjang_barang,new_lebar_barang,new_tinggi_barang,new_massa_barang,new_volumetrik_pengiriman,new_jumlah_barang,id_barang,nama_barang,panjang_barang,lebar_barang,tinggi_barang,massa_barang,volumetrik_pengiriman,jumlah_barang):
+	c.execute("UPDATE data_rincian_barang SET id_barang =?,nama_barang=?,panjang_barang=?,lebar_barang=?,tinggi_barang=?,massa_barang=?,volumetrik_pengiriman=?,jumlah_barang=? WHERE id_barang =? and nama_barang=? and panjang_barang=? and lebar_barang=? and tinggi_barang=? and volumetrik_pengiriman=? and jumlah_barang=? ",(new_id_barang,new_nama_barang,new_panjang_barang,new_lebar_barang,new_tinggi_barang,new_massa_barang,new_volumetrik_pengiriman,new_jumlah_barang,id_barang,nama_barang,panjang_barang,lebar_barang,tinggi_barang,massa_barang,volumetrik_pengiriman,jumlah_barang))
 	conn.commit()
 	data = c.fetchall()
 	return data
 
-def delete_data(nama_rincian_barang):
-	c.execute('DELETE FROM data_rincian_barang WHERE nama_rincian_barang="{}"'.format(nama_rincian_barang))
+def delete_data(nama_barang):
+	c.execute('DELETE FROM data_rincian_barang WHERE nama_barang="{}"'.format(nama_barang))
 	conn.commit()
