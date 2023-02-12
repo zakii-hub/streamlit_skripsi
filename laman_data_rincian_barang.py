30cd44edc99861b445d5e2ed7cea39d477f29ebc
Muhammad Zakii Rofii <57617074+zakii-hub@users.noreply.github.com>
2023-02-13 02:35:51 +0700

    Penambahan Fitur CRUD Laman Data Rincian Barang

--- laman_data_rincian_barang.py
+++ laman_data_rincian_barang.py
@@ -9,10 +9,10 @@ from st_btn_select import st_btn_select
 import plotly.express as px 
 
 def laman_data_rincian_barang():
-    st.subheader("Semua Data rincian_barang (Kantor)")
+    st.subheader("Semua Data Rincian Barang")
     result = view_all_data_rincian_barang()
     # st.write(result)
-    clean_df = pd.DataFrame(result,columns=["id_rincian_barang","nama_rincian_barang","status_rincian_barang","longitude_rincian_barang","latitude_rincian_barang","alamat_rincian_barang"])
+    clean_df = pd.DataFrame(result,columns=["id_barang","nama_barang","panjang_barang","lebar_barang","tinggi_barang","massa_barang","volumetrik_pengiriman","jumlah_barang"])
     st.dataframe(clean_df)
 
     # st.subheader("Task Status")
@@ -31,23 +31,23 @@ def laman_data_rincian_barang():
         
 
     if option=='Tambah Data':
-        st.subheader("Tambah Data rincian_barang")
+        st.subheader("Tambah Data Rincian Barang")
         col1,col2 = st.columns(2)
         
         with col1:
-            id_rincian_barang = st.text_input("Id rincian_barang")
-            nama_rincian_barang = st.text_input("Nama rincian_barang")
-            status_rincian_barang = st.selectbox("Status",["Active","In-Active"])
-
+            id_barang = st.text_input("Id Barang")
+            nama_barang = st.text_input("Nama Barang")
+            panjang_barang = st.number_input("Panjang Barang", step=1)
+            lebar_barang = st.number_input("Lebar Barang", step=1)
+            tinggi_barang = st.number_input("Tinggi Barang", step=1)
         with col2:
-            longitude_rincian_barang = st.number_input("Longitude rincian_barang",step=1e-15,format="%.15f")
-            latitude_rincian_barang = st.number_input("Latitude rincian_barang",step=1e-15,format="%.15f")
+            massa_barang = st.number_input("Massa Barang",step=1)
+            volumetrik_pengiriman = st.number_input("Volumetrik Barang",step=1)
+            jumlah_barang = st.number_input("Jumlah Barang",step=1)
         
-        alamat_rincian_barang = st.text_area("Alamat rincian_barang")
-
         if st.button("Tambah Data"):
-            add_data_rincian_barang(id_rincian_barang,nama_rincian_barang,status_rincian_barang,longitude_rincian_barang,latitude_rincian_barang,alamat_rincian_barang)
-            st.success("Berhasil menambahkan ::{} ::ke data rincian_barang".format(nama_rincian_barang))
+            add_data_rincian_barang(id_barang,nama_barang,panjang_barang,lebar_barang,tinggi_barang,massa_barang,volumetrik_pengiriman,jumlah_barang)
+            st.success("Berhasil menambahkan ::{} ::ke data rincian_barang".format(nama_barang))
 
     if option=='Ubah Data':
         st.subheader("Ubah Data rincian_barang")
@@ -57,26 +57,31 @@ def laman_data_rincian_barang():
         # st.write(task_result)
 
         if nama_rincian_barang_result:
-            id_rincian_barang = nama_rincian_barang_result [0][0]
-            nama_rincian_barang = nama_rincian_barang_result[0][1]
-            status_rincian_barang = nama_rincian_barang_result[0][2]
-            longitude_rincian_barang = nama_rincian_barang_result[0][3]
-            latitude_rincian_barang = nama_rincian_barang_result[0][4]
-            alamat_rincian_barang = nama_rincian_barang_result[0][5]
+            id_barang = nama_rincian_barang_result [0][0]
+            nama_barang = nama_rincian_barang_result[0][1]
+            panjang_barang = nama_rincian_barang_result[0][2]
+            lebar_barang = nama_rincian_barang_result[0][3]
+            tinggi_barang = nama_rincian_barang_result[0][4]
+            massa_barang = nama_rincian_barang_result[0][5]
+            volumetrik_pengiriman = nama_rincian_barang_result[0][6]
+            jumlah_barang = nama_rincian_barang_result[0][7]
 
             col1,col2 = st.columns(2)
                 
             with col1:
-                new_id_rincian_barang = st.text_input("Id rincian_barang",id_rincian_barang)
-                new_nama_rincian_barang = st.text_input("Nama rincian_barang",nama_rincian_barang)
-                new_status_rincian_barang = st.selectbox(status_rincian_barang,["Active","In-Active"])
+                new_id_barang = st.text_input("Id Barang",id_barang)
+                new_nama_barang = st.text_input("Nama Barang",nama_barang)
+                new_panjang_barang = st.number_input("Panjang Barang",panjang_barang,step=1)
+                new_lebar_barang = st.number_input("Lebar Barang",lebar_barang,step=1)
+                new_tinggi_barang = st.number_input("Tinggi Barang",tinggi_barang,step=1)
+
             with col2:
-                new_longitude_rincian_barang = st.number_input("Longitude rincian_barang",step=1e-15,format="%.15f")
-                new_latitude_rincian_barang = st.number_input("Latitude rincian_barang",step=1e-15,format="%.15f")
-            new_alamat_rincian_barang = st.text_area("Alamat rincian_barang",alamat_rincian_barang)
+                new_massa_barang = st.number_input("Massa Barang",massa_barang,step=1)
+                new_volumetrik_pengiriman = st.number_input("Volumetrik Pengiriman",volumetrik_pengiriman,step=1)
+                new_jumlah_barang = st.number_input("Jumlah Barang",jumlah_barang,step=1)
             if st.button("Update Task"):
-                edit_data_rincian_barang(new_id_rincian_barang,new_nama_rincian_barang,new_status_rincian_barang,new_longitude_rincian_barang,new_latitude_rincian_barang,new_alamat_rincian_barang,id_rincian_barang,nama_rincian_barang,status_rincian_barang,longitude_rincian_barang,latitude_rincian_barang,alamat_rincian_barang)
-                st.success("Updated ::{} ::To {}".format(nama_rincian_barang,new_nama_rincian_barang))
+                edit_data_rincian_barang((new_id_barang,new_nama_barang,new_panjang_barang,new_lebar_barang,new_tinggi_barang,new_massa_barang,new_volumetrik_pengiriman,new_jumlah_barang,id_barang,nama_barang,panjang_barang,lebar_barang,tinggi_barang,massa_barang,volumetrik_pengiriman,jumlah_barang))
+                st.success("Updated ::{} ::To {}".format(nama_barang,new_nama_barang))
 
     if option=='Hapus Data':
         st.subheader("Hapus Data rincian_barang")
